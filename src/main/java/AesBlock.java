5811630ebc6bae3e4c2cf0b7526444850ef49982
amiados <amiad.ard@gmail.com>
2025-09-21 20:10:21 +0300

    20:10 21/9/25

--- src/main/java/AesBlock.java
+++ src/main/java/AesBlock.java
@@ -2,4 +2,5 @@ public interface AesBlock {
     void setKey(byte[] key);
     void encryptKey(byte[] in, int inOff, byte[] out, int outOff);
     void decryptKey(byte[] in, int inOff, byte[] out, int outOff);
+
 }
